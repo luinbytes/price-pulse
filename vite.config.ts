11216afa669983ee4a8ce13575726c0cdd1eec5e
Lu <42706009+luinbytes@users.noreply.github.com>
2026-01-11 21:42:26 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/price-tracker/", // For GitHub Pages
+  base: "/price-pulse/", // For GitHub Pages
 })
