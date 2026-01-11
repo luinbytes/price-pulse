afa86df98a3de986a62655543b2c92ccb8eb142a
Lu <42706009+luinbytes@users.noreply.github.com>
2026-01-11 20:04:51 +0000

    Update price-worker.ts

--- scripts/price-worker.ts
+++ scripts/price-worker.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+dotenv.config({ path: '.env.local' })
 import { createClient } from '@supabase/supabase-js'
 import puppeteer from 'puppeteer'
 
